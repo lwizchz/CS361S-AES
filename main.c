dbbf71dd7a32c7dacaee271c6885b596a1bba3cd
Andrew Q Langman <andrewqlangman@utexas.edu>
2018-09-14 14:53:12 -0500

    Add preliminary file reading

--- main.c
+++ main.c
@@ -5,7 +5,29 @@
 int main(int argc, char** argv) {
     printf("Hello, AES!\n");
 
-    handleArgs(argc, argv);
+    Options opt = handleArgs(argc, argv);
 
-    return 0;
+    State state_ptr* = (State*) malloc(sizeof(State));
+    
+    FILE *fptr = NULL;
+
+    if ((fptr = fopen(opt.inputfile, "rb")) == NULL)
+    {
+        printf("ERROR OPENING FILE");
+        exit (1);
+    }
+
+    for (int c = 0; c < 4; c++)
+    {
+        for (int r = 0; r < 4; r++)
+        {
+            fread(*state_ptr[r][c], sizeof(char), 1, fptr);
+        }
+        
+    }
+
+    fclose(fptr);
+
+    fprintf("First byte: %x\n", *state_ptr[0][0]);
+
 }
