9160020cec1f6f00feef310fdf594284d2612eeb
Luke Montalvo <lukemontalvo@gmail.com>
2018-09-15 15:51:43 -0500

    Add aes_sbox and aes_inv_sbox arrays for use in subBytes()

--- main.c
+++ main.c
@@ -7,15 +7,7 @@
 int main(int argc, char** argv) {
     Options opt = handleArgs(argc, argv);
 
-    State* state_ptr = malloc(sizeof(State));
-    memset(state_ptr->byte, 0, sizeof(State));
-
-    readStates(opt.inputfile);
-
-    
-    
-
-    //fclose(fptr);
+    State** state_array = readStates(opt.inputfile);
 
     return 0;
 }
